ea08e12b68f5e6f8d69ae96ec66eb2e596ca9fdc
Harry <hengwang@cn.ibm.com>
2019-09-01 10:58:10 +0800

    Initial commit

---
+++ src/com/imooc/FloatDemo.java
@@ -0,0 +1,15 @@
+package com.imooc;
+
+public class FloatDemo {
+   public static void main(String[] args){
+        float f = 1234.324f;
+        char a = 'a';
+        char ch = 122;
+//        for (int i=0;i<66;i++){
+//            System.out.println((char)i);
+//        }
+       char c = '\u005d';
+       System.out.println("c="+c);
+        System.out.println("f=" + f + '/' + a + '/' + ch);
+    }
+}
