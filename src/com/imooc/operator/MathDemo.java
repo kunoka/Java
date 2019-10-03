33283ca6c2ba11a866257818cbf36628897c359a
Harry <hengwang@cn.ibm.com>
2019-10-03 09:07:52 +0800

    Java零基础入门⁩ ▸ ⁨步骤一Java基本语法⁩ ▸ ⁨三、Java运算符⁩ 3-3if-else条件结构

--- src/com/imooc/operator/MathDemo.java
+++ src/com/imooc/operator/MathDemo.java
@@ -1,11 +1,16 @@
 package com.imooc.operator;
 
+import java.util.Scanner;
+
 public class MathDemo {
     public static void main(String[] args) {
-        int num1=10, num2=25;
-        int result;
-        result = num1 + num2;
-        System.out.println(num1 + "+" + num2 + "="+result);
-        System.out.println(""+num1+num2);
+        System.out.println("请输入一个整数：");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if(n%2==0){
+            System.out.println("n是偶数");
+        }else{
+            System.out.println("n是奇数");
+        }
     }
 }
