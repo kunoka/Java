fcb9f5ca256c950df40d9758e40cdf785726f4f2
Harry <hengwang@cn.ibm.com>
2019-10-04 08:33:58 +0800

    6-1闰年问题案例

---
+++ src/com/imooc/operator/MaxDemo.java
@@ -0,0 +1,10 @@
+package com.imooc.operator;
+
+public class MaxDemo {
+    public static void main(String[] args) {
+//        int max;
+        int a = 190,b=30;
+        int max = a>b ? a: b;
+        System.out.println("max:" + max);
+    }
+}
